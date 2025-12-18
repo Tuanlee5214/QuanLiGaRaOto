433e49511ef2b2cf69df71df97ee76cd62bf1efe
TuanLee <23521711@gm.uit.edu.vn>
2025-12-18 10:16:27 +0700

    Add main window for project

--- QlyGaraOto/QuanLiGaRaOto/QuanLiGaRaOto/Program.cs
+++ QlyGaraOto/QuanLiGaRaOto/QuanLiGaRaOto/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLiGaRaOto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindoww());
         }
     }
 }
