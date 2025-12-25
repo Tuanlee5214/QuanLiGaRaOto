72f213801753e04bfbc8242899cfd0f6d0796e75
TuanLee <23521711@gm.uit.edu.vn>
2025-12-26 00:43:33 +0700

    Add ImportOrderInfo

--- QlyGaraOto/QuanLiGaRaOto/QuanLiGaRaOto/ImportOrder.cs
+++ QlyGaraOto/QuanLiGaRaOto/QuanLiGaRaOto/ImportOrder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLiGaRaOto.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,5 +27,153 @@ namespace QuanLiGaRaOto
         {
             this.Close();
         }
+
+        private void LoadForm(object sender, EventArgs e)
+        {
+            this.textBox1.Text = ImportOrderService.Instance.GetMaPhieuNhap();
+            this.comboBox3.DataSource = RepairFormService.Instance.GetPhuTung();
+            this.comboBox3.DisplayMember = "TenPhuTung";
+            this.comboBox3.ValueMember = "MaPhuTung";
+
+            this.comboBox3.SelectedValue = "";
+        }
+
+        private void TextChange(object sender, EventArgs e)
+        {
+            decimal output;
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox4.Text = "";
+                return;
+            }
+            if (!Decimal.TryParse(textBox2.Text, out output) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Vui lòng chỉ nhập số", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                return;
+            }
+            if(!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox4.Text = Convert.ToString(numericUpDown1.Value * Convert.ToDecimal(textBox2.Text));
+            }
+        }
+
+        private void ValueChange(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox4.Text = "";
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox4.Text = Convert.ToString(numericUpDown1.Value * Convert.ToDecimal(textBox2.Text));
+            }
+        }
+
+        private void AddInfoIntoDatagrid(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(comboBox3.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Tên phụ tùng và đơn giá không được bỏ trống", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string mavt = comboBox3.SelectedValue.ToString();
+            string tenvt = comboBox3.Text;
+            string sl = numericUpDown1.Value.ToString();
+            string dg = textBox2.Text;
+            string tt = textBox4.Text;
+            dataGridView1.Rows.Add(mavt, tenvt, sl, dg, tt);
+            TinhTongTien();
+        }
+
+        private void DeleteFromDatagrid(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null || dataGridView1.CurrentRow.IsNewRow)
+            {
+                MessageBox.Show("Vui lòng chọn dòng cần xóa", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var confirm = MessageBox.Show(
+                "Bạn có chắc muốn xóa dòng này?",
+                "Xác nhận",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question);
+
+            if (confirm != DialogResult.OK) return;
+
+            dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+
+            // Tính lại tổng tiền sau khi xóa
+            TinhTongTien();
+        }
+        private void TinhTongTien()
+        {
+            decimal tongThanhTienPhuTung = 0;
+
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                // 1. Cộng thành tiền phụ tùng
+                decimal thanhTienPT = Convert.ToDecimal(row.Cells["ThanhTien"].Value);
+                tongThanhTienPhuTung += thanhTienPT;
+            }
+
+            decimal tongTien = tongThanhTienPhuTung;
+            textBox6.Text = tongTien.ToString("N0");
+        }
+
+        private void LapPhieuNhap(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng thêm thông tin", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string mapn = textBox1.Text;
+            DateTime ngaynhap = dateTimePicker1.Value;
+            decimal tongtien = Convert.ToDecimal(textBox6.Text);
+            ImportOrderService.Instance.InsertIntoPhieuNhap(mapn, ngaynhap, tongtien);
+
+            foreach (DataGridViewRow dataRow in dataGridView1.Rows)
+            {
+                if (dataRow.IsNewRow) continue;
+
+                string mavt = dataRow.Cells["MaVTPT"].Value?.ToString();
+
+                int sl = dataRow.Cells["SoLuong"].Value == null
+                    ? 0
+                    : Convert.ToInt32(dataRow.Cells["SoLuong"].Value);
+
+                decimal dg = dataRow.Cells["DonGia"].Value == null
+                    ? 0
+                    : Convert.ToDecimal(dataRow.Cells["DonGia"].Value);
+
+                decimal tt = dataRow.Cells["ThanhTien"].Value == null
+                    ? 0
+                    : Convert.ToDecimal(dataRow.Cells["ThanhTien"].Value);
+
+                ImportOrderService.Instance.InsertIntoCT_PhieuNhap(mapn, mavt, sl, dg, tt);
+                var pt = RepairFormService.Instance.GetPhuTungInfo(mavt);
+                int slcu = pt.SoLuongTon;
+                decimal dgnhapcu = pt.DonGiaNhap;
+                int slmoi = slcu + sl;
+                RepairFormService.Instance.UpdateSoLuong(mavt, slmoi);
+                if(dgnhapcu != dg)
+                {
+                    double tilelai = RuleService.Instance.GetTiLeLai();
+                    decimal dgmoi = (decimal)tilelai * dg;
+                    ImportOrderService.Instance.UpdateDonGiaPT(mavt, dg, dgmoi);
+                }
+            }
+            MessageBox.Show("Thêm thông tin thành công");
+            this.Close();
+        }
     }
+    
 }
---
+++ QlyGaraOto/QuanLiGaRaOto/QuanLiGaRaOto/Service/ImportOrderService.cs
@@ -0,0 +1,178 @@
+﻿using QuanLiGaRaOto.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiGaRaOto.Service
+{
+    public class ImportOrderService
+    {
+        private readonly DatabaseConnection _db = new DatabaseConnection();
+
+        private static ImportOrderService instance;
+        public static ImportOrderService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ImportOrderService();
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+
+        public string GetMaPhieuNhap()
+        {
+            using (var conn = _db.GetConnection())
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT MAX(MaPhieuNhap) FROM PHIEUNHAPPHUTUNG";
+
+                object result = cmd.ExecuteScalar();
+
+                int nextNumber = 1;
+
+                if (result != null && result != DBNull.Value)
+                {
+                    string lastCode = result.ToString();
+                    int number = int.Parse(lastCode.Substring(2));
+                    nextNumber = number + 1;
+                }
+
+                return "PN" + nextNumber.ToString("D5");
+            }
+        }
+
+        public InsertOrUpdateResult InsertIntoPhieuNhap(string mapn, DateTime ngaynhap, decimal tongtien)
+        {
+            try
+            {
+                using (var conn = _db.GetConnection())
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO PHIEUNHAPPHUTUNG (MaPhieuNhap, NgayNhap, TongTien) " +
+                                      "VALUES(@mapn, @ngaynhap, @tongtien)";
+                    cmd.Parameters.Add("@mapn", SqlDbType.Char, 7).Value = mapn;
+                    cmd.Parameters.Add("@ngaynhap", SqlDbType.DateTime).Value = ngaynhap;
+                    cmd.Parameters.Add("@tongtien", SqlDbType.Money).Value = tongtien;
+
+
+                    int row = cmd.ExecuteNonQuery();
+                    if (row != 0)
+                    {
+                        return new InsertOrUpdateResult
+                        {
+                            Success = true,
+                            SuccessMessage = "Thêm thông tin thành công"
+                        };
+                    }
+                    else
+                    {
+                        return new InsertOrUpdateResult
+                        {
+                            Success = false,
+                            ErrorMessage = "Thêm thông tin thất bại"
+                        };
+                    }
+
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+                return new InsertOrUpdateResult
+                {
+                    Success = false,
+                    ErrorMessage = "Lỗi kết nối tới máy chủ"
+                };
+            }
+        }
+
+        public InsertOrUpdateResult InsertIntoCT_PhieuNhap(string mapn, string mapt, int sl, decimal dongia, decimal tt)
+        {
+            try
+            {
+                using (var conn = _db.GetConnection())
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                @"INSERT INTO CT_PHIEUNHAPPHUTUNG
+                  (MaPhieuNhap, MaPhuTung, SoLuongNhap, DonGiaNhap, ThanhTien)
+                  VALUES
+                  (@mapn, @mapt, @sl, @dongia, @tt)";
+
+                    cmd.Parameters.Add("@mapn", SqlDbType.Char, 7).Value = mapn;
+                    cmd.Parameters.Add("@mapt", SqlDbType.Char, 7).Value = mapt;
+                    cmd.Parameters.Add("@sl", SqlDbType.Int).Value = sl;
+                    cmd.Parameters.Add("@dongia", SqlDbType.Money).Value = dongia;
+                    cmd.Parameters.Add("@tt", SqlDbType.Money).Value = tt;
+
+                    int row = cmd.ExecuteNonQuery();
+
+                    return new InsertOrUpdateResult
+                    {
+                        Success = row > 0,
+                        SuccessMessage = row > 0 ? "Thêm ct phiếu nhập thành công" : null,
+                        ErrorMessage = row > 0 ? null : "Thêm ct phiếu nhập thất bại"
+                    };
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+                return new InsertOrUpdateResult
+                {
+                    Success = false,
+                    ErrorMessage = "Lỗi kết nối tới máy chủ"
+                };
+            }
+        }
+
+        public InsertOrUpdateResult UpdateDonGiaPT(string mapt, decimal dgnhapmoi, decimal dgbanmoi)
+        {
+            try
+            {
+                using (var conn = _db.GetConnection())
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE PHUTUNG SET DonGiaNhap = @dgn, DonGiaBan = @dgb WHERE MaPhuTung = @mapt";
+
+
+                    cmd.Parameters.Add("@mapt", SqlDbType.Char, 7).Value = mapt;
+                    cmd.Parameters.Add("@dgn", SqlDbType.Money).Value = dgnhapmoi;
+                    cmd.Parameters.Add("@dgb", SqlDbType.Money).Value = dgbanmoi;
+
+
+
+                    int row = cmd.ExecuteNonQuery();
+
+                    return new InsertOrUpdateResult
+                    {
+                        Success = row > 0,
+                        SuccessMessage = row > 0 ? "Sửa chữa thành công" : null,
+                        ErrorMessage = row > 0 ? null : "Sửa chữa thất bại"
+                    };
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+                return new InsertOrUpdateResult
+                {
+                    Success = false,
+                    ErrorMessage = "Lỗi kết nối tới máy chủ"
+                };
+            }
+        }
+    }
+}
